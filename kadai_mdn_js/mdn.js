a3c3c051bd1932c9ed7ef67602500a3b89cebcb5
kei-otomo <kei.otomo@ajiko.co.jp>
2025-10-15 17:24:37 +0900

    Update mdn.js

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,7 +1,7 @@
 const timeNow = new Date()
 
 let year = timeNow.getFullYear()
-let month = timeNow.getMonth()
+let month = timeNow.getMonth() + 1
 let date = timeNow.getDate()
 
 console.log(year + "年" + month + "月" + date + "日")
